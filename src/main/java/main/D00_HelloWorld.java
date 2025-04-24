3a92fd482b92c1b79271f296775785fa4afc2f8d
Daniel Appenmaier <daniel.appenmaier@gmail.com>
2025-04-24 17:17:03 +0200

    Update Hello World

--- src/main/java/main/D00_HelloWorld.java
+++ src/main/java/main/D00_HelloWorld.java
@@ -10,7 +10,7 @@ package main;
 public class D00_HelloWorld {
 
    public static void main(String[] args) {
-      System.out.println("Hello World");
+      System.out.println("Hello World, How Are U?");
    }
 
 }
